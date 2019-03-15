b67dcafaf9e56f30d4a28327b88af5835ba41e30
rango <nneopane@oswego.edu>
2019-03-14 21:43:06 -0400

    added new fucn to populate dynamo db

--- main.go
+++ main.go
@@ -9,67 +9,68 @@ import (
 	"os"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	loggly "github.com/jamespearly/loggly"
 )
 
-// Response structs
+type ObjPlayer struct {
+	ID             int         `json:"id"`
+	Name           string      `json:"name"`
+	FirstName      string      `json:"firstName"`
+	LastName       interface{} `json:"lastName"`
+	DateOfBirth    string      `json:"dateOfBirth"`
+	CountryOfBirth string      `json:"countryOfBirth"`
+	Nationality    string      `json:"nationality"`
+	Position       string      `json:"position"`
+	ShirtNumber    int         `json:"shirtNumber"`
+	LastUpdated    time.Time   `json:"lastUpdated"`
+}
+type ObjArea struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+type ObjCompetition struct {
+	ID          int       `json:"id"`
+	Area        ObjArea   `json:"area"`
+	Name        string    `json:"name"`
+	Code        string    `json:"code"`
+	Plan        string    `json:"plan"`
+	LastUpdated time.Time `json:"lastUpdated"`
+}
+
+type ObjSeason struct {
+	ID              int         `json:"id"`
+	StartDate       string      `json:"startDate"`
+	EndDate         string      `json:"endDate"`
+	CurrentMatchday int         `json:"currentMatchday"`
+	Winner          interface{} `json:"winner"`
+}
+
+type ObjTeam struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 type Response struct {
 	Count   int `json:"count"`
 	Filters struct {
 		Limit int `json:"limit"`
 	} `json:"filters"`
-
-	// name of competitions
-	Competition struct {
-		ID   int `json:"id"`
-		Area struct {
-			ID   int    `json:"id"`
-			Name string `json:"name"`
-		} `json:"area"`
-		Name        string    `json:"name"`
-		Code        string    `json:"code"`
-		Plan        string    `json:"plan"`
-		LastUpdated time.Time `json:"lastUpdated"`
-	} `json:"competition"`
-
-	// seasonYY/YY
-	Season struct {
-		ID              int         `json:"id"`
-		StartDate       string      `json:"startDate"`
-		EndDate         string      `json:"endDate"`
-		CurrentMatchday int         `json:"currentMatchday"`
-		Winner          interface{} `json:"winner"`
-	} `json:"season"`
-
-	//players info
-	Scorers []struct {
-		Player struct {
-			ID             int         `json:"id"`
-			Name           string      `json:"name"`
-			FirstName      string      `json:"firstName"`
-			LastName       interface{} `json:"lastName"`
-			DateOfBirth    string      `json:"dateOfBirth"`
-			CountryOfBirth string      `json:"countryOfBirth"`
-			Nationality    string      `json:"nationality"`
-			Position       string      `json:"position"`
-			ShirtNumber    int         `json:"shirtNumber"`
-			LastUpdated    time.Time   `json:"lastUpdated"`
-		} `json:"player"`
-
-		// name of the club
-		Team struct {
-			ID   int    `json:"id"`
-			Name string `json:"name"`
-		} `json:"team"`
-		// socreas
-		NumberOfGoals int `json:"numberOfGoals"`
+	Competition ObjCompetition `json:"competition"`
+	Season      ObjSeason      `json:"season"`
+	Scorers     []struct {
+		Player        ObjPlayer `json:"player"`
+		Team          ObjTeam   `json:"team"`
+		NumberOfGoals int       `json:"numberOfGoals"`
 	} `json:"scorers"`
 }
 
 func printTop10Scorer(t time.Time) {
 	//loogly token // TODO: set in the enviroment for security
-	//os.Setenv("LOGGLY_TOKEN", "4156602e-1451-4806-a3ad-80c982025fb1")
-	os.Getenv("LOGGLY_TOKEN")
+	//	os.Setenv("LOGGLY_TOKEN", "")
 	tag := "SoccerScore"
 
 	// Instantiate the loogly Client
@@ -78,7 +79,7 @@ func printTop10Scorer(t time.Time) {
 	// API end point urs (add nultiples)
 	url := "https://api.football-data.org/v2/competitions/PD/scorers"
 	req, err := http.NewRequest("GET", url, nil)
-	req.Header.Add("X-Auth-Token", "e42cb6a6ecc949c8897e06d284a55e05")
+	req.Header.Add("X-Auth-Token", os.Getenv("API_AUTH_TOKEN"))
 
 	// Valid EchoSend (message echoed to console and no error returned)
 	logerr := looglyClient.EchoSend("info", "accessing json objects")
@@ -105,19 +106,7 @@ func printTop10Scorer(t time.Time) {
 		logerr = looglyClient.Send("error", "object not found")
 		fmt.Println("err:", logerr)
 	}
-	fmt.Print("Top ")
-	fmt.Print(responseObject.Count)
-	fmt.Print(" Scorers")
-	fmt.Println()
-	fmt.Println(responseObject.Competition.Name)
-	fmt.Println("Name" + "\t\t\t" + "Goals")
-
-	for i := 0; i < len(responseObject.Scorers); i++ {
-		fmt.Print(responseObject.Scorers[i].Player.Name + "\t\t")
-		fmt.Print(responseObject.Scorers[i].NumberOfGoals)
-		fmt.Println()
-
-	}
+	insertIntoDynamoDB(responseObject)
 
 }
 
@@ -128,16 +117,41 @@ func doEvery(d time.Duration, f func(time.Time)) {
 	}
 }
 
+func insertIntoDynamoDB(res Response) {
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	// insert players into db
+	for i := 0; i < len(res.Scorers); i++ {
+		av, err := dynamodbattribute.MarshalMap(res.Scorers[i].Player)
+		if err != nil {
+			fmt.Println("Got error marshalling map:")
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		input := &dynamodb.PutItemInput{
+			Item:      av,
+			TableName: aws.String("Top10"),
+		}
+
+		_, err = svc.PutItem(input)
+
+		if err != nil {
+			fmt.Println("Got error calling PutItem:")
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("Successfully added player")
+	}
+}
+
 // main function to start the program
 func main() {
-	//doEvery(10*time.Second, printTop10Scorer)
-	print("Hello Ticker")
-
-	ticker := time.NewTimer(time.Second * 10)
-
-	go func() {
-		for t := range ticker.C {
-			fmt.Println("Tick At", t)
-		}
-	}()
+	doEvery(10*time.Second, printTop10Scorer)
 }
